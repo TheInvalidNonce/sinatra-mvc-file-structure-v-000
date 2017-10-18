61acd5d0db489b852f5b3b17c46882758780d43b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-18 19:35:30 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,12 +2,13 @@ class Dog
   attr_accessor :name, :breed, :age
 
   @@all = []
-  
+
   def initialize(name, breed, age)
     # attrs.map { |k, v| self.send("#{k}=", v)}
     @name = name
     @breed = breed
     @age = age
+    @@all << self
   end
 
   def self.all
